c0e846bdec9191525cba6f56b4269dffc9eb4f3b
Connor Flowers <cjflowers@pm.me>
2025-08-02 12:01:54 +1000

    add content config

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z } from 'astro:content';
+
+const projectsCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    description: z.string(),
+    publishDate: z.date(),
+    heroImage: image(),
+    liveUrl: z.string().url().optional(),
+    repoUrl: z.string().url().optional(),
+    technologies: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+  'projects': projectsCollection,
+};
